a572236775548eef7db09315499ebeec2ba301e5
Ziad ElNady <ziadelnady555@gmail.com>
2024-12-21 21:07:20 -0800

    Initial Commit

---
+++ base-template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base-template
+//
+//  Created by Ziad Ahmed on 21/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
